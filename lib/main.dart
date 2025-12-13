e6383124464468ac14200ee927b27638d34e63c6
matthias122 <ahrensmatthias122@gmail.com>
2025-12-13 17:01:59 +0100

    Update comment in main.dart for clarity on AuthCheckScreen usage

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // Starte mit dem AuthCheckScreen, um den Login-Status zu prüfen
+      // Start with the AuthCheckScreen to check the login status.
       home: const AuthCheckScreen(),
     );
   }
