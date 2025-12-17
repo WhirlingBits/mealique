8af908dbaa10f61082192c04f3ba292d0a08c726
matthias122 <ahrensmatthias122@gmail.com>
2025-12-17 12:22:32 +0100

    Add LocaleProvider for managing app localization

---
+++ lib/providers/locale_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mealique/l10n/app_localizations.dart';
+
+class LocaleProvider extends ChangeNotifier {
+  Locale _locale;
+
+  // Default system language or fallback to English
+  LocaleProvider() : _locale = const Locale('en');
+
+  Locale get locale => _locale;
+
+  void setLocale(Locale locale) {
+    if (!AppLocalizations.supportedLocales.contains(locale)) return;
+    _locale = locale;
+    notifyListeners();
+  }
+}
