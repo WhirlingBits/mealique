92299e6c3ed0206fa16d1a61f441ef29aa413877
matthias122 <ahrensmatthias122@gmail.com>
2026-02-14 21:10:13 +0100

    Add detailed actions menu for shopping list with new options

--- lib/ui/widgets/shopping_list_detail_actions_menu.dart
+++ lib/ui/widgets/shopping_list_detail_actions_menu.dart
@@ -12,8 +12,62 @@ class ShoppingListDetailActionsMenu extends StatelessWidget {
       },
       itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
         const PopupMenuItem<String>(
-          value: 'clear_checked',
-          child: Text('Erledigte entfernen'), // TODO: l10n
+          value: 'refresh',
+          child: ListTile(
+            leading: Icon(Icons.refresh),
+            title: Text('Aktualisieren'), // TODO: l10n
+          ),
+        ),
+        const PopupMenuItem<String>(
+          value: 'edit_list',
+          child: ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Liste bearbeiten'), // TODO: l10n
+          ),
+        ),
+        const PopupMenuItem<String>(
+          value: 'toggle_completed',
+          child: ListTile(
+            leading: Icon(Icons.check_box_outline_blank),
+            title: Text('Abgeschlossene Items anzeigen'), // TODO: l10n
+          ),
+        ),
+        const PopupMenuItem<String>(
+          value: 'sort',
+          child: ListTile(
+            leading: Icon(Icons.sort),
+            title: Text('Sortieren'), // TODO: l10n
+          ),
+        ),
+         const PopupMenuItem<String>(
+          value: 'toggle_categories',
+          child: ListTile(
+            leading: Icon(Icons.label_off),
+            title: Text('Kategorien ausblenden'), // TODO: l10n
+          ),
+        ),
+        const PopupMenuDivider(),
+        const PopupMenuItem<String>(
+          value: 'uncheck_all',
+          child: ListTile(
+            leading: Icon(Icons.radio_button_unchecked),
+            title: Text('Markierung aller Elemente entfernen'), // TODO: l10n
+          ),
+        ),
+        const PopupMenuItem<String>(
+          value: 'delete_completed',
+          child: ListTile(
+            leading: Icon(Icons.delete_sweep),
+            title: Text('Erledigte Elemente löschen'), // TODO: l10n
+          ),
+        ),
+        const PopupMenuDivider(),
+        const PopupMenuItem<String>(
+          value: 'delete_list',
+          child: ListTile(
+            leading: Icon(Icons.delete_forever, color: Colors.red),
+            title: Text('Liste löschen', style: TextStyle(color: Colors.red)), // TODO: l10n
+          ),
         ),
       ],
     );
