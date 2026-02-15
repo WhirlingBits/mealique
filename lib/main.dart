373904d626c38f096c57fc9c1b3cf3903d5e333c
matthias122 <ahrensmatthias122@gmail.com>
2026-02-15 11:21:08 +0100

    Add locale resolution callback to support device language preferences

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,25 @@ class MyApp extends StatelessWidget {
             darkTheme: ThemeData.dark(),
             themeMode: settings.themeMode,
             locale: settings.locale,
+            localeResolutionCallback: (deviceLocale, supportedLocales) {
+              // If the user has already chosen a language, use that.
+              if (settings.locale != null) {
+                return settings.locale;
+              }
+              // Otherwise, determine the initial locale based on the device's language.
+              if (deviceLocale != null) {
+                for (var supportedLocale in supportedLocales) {
+                  if (supportedLocale.languageCode == deviceLocale.languageCode) {
+                    // If device language is German, use it.
+                    if (deviceLocale.languageCode == 'de') {
+                      return supportedLocale;
+                    }
+                  }
+                }
+              }
+              // If the device language is not German or not supported, default to English.
+              return supportedLocales.firstWhere((l) => l.languageCode == 'en');
+            },
             localizationsDelegates: const [
               AppLocalizations.delegate,
               GlobalMaterialLocalizations.delegate,
